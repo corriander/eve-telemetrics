3bee887f08d2906637b0f30224a3e95b1b4662a9
Alex Corrie <ajccode@gmail.com>
2018-06-11 00:00:58 +0100

    Add lazy database connection instantiation
    A new property `db` becomes a read-only database connection when first
    called and is cached for future usage.

--- evetele/esd.py
+++ evetele/esd.py
@@ -12,6 +12,26 @@ from . import config
 
 class Database(object):
 
+    _pw_prompt = "Password for [{user}@{host}:{port}/{database}]: "
+
+    default_cursor_factory = psycopg2.extras.NamedTupleCursor
+
     @property
     def conn_details(self):
         return dict(config.items('ESD Source'))
+
+    @property
+    def db(self):
+        try:
+            return self._db
+        except AttributeError:
+            if 'password' not in self.conn_details:
+                password = getpass.getpass(
+                    self._pw_prompt.format(**self.conn_details)
+                )
+            self._db = connection = psycopg2.connect(
+                cursor_factory=self.default_cursor_factory,
+                **self.conn_details
+            )
+            connection.set_session(autocommit=True, readonly=True)
+            return connection
--- evetele/tests/test_esd.py
+++ evetele/tests/test_esd.py
@@ -1,11 +1,12 @@
 import os
 import unittest
+from unittest import mock
 
 import evetele
 from evetele import esd
 
 
-class Database(unittest.TestCase):
+class TestDatabase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -16,9 +17,46 @@ class Database(unittest.TestCase):
         assert evetele.config.config_paths == path
 
     def test_conn_details(self):
-        """Property contains connection details for the database."""
-        db = esd.Database()
-        self.assertEqual(db.conn_details['host'], 'localhost')
+        """Property contains connection details for the database.
+
+        The connection details are sourced from the config file(s)
+        located in the usual app dirs (dependent on OS).
+        """
+        database = esd.Database()
+        self.assertEqual(database.conn_details['host'], 'localhost')
+
+    @mock.patch('psycopg2.connect')
+    def test_db(self, stub_connect):
+        """Property lazily instantiates a connection object.
+
+        After creating a connection, the session is set to readonly
+        and autocommit to avoid issues with hanging transactions (only
+        SELECT is used; we don't want to modify the static data).
+
+        Note: the db property also prompts for a password on creating
+        a new connection if it hasn't been provided in the config but
+        this isn't tested here.
+        """
+        database = esd.Database()
+
+        self.assertFalse(stub_connect.called)
+
+        value = database.db
+
+        # Check the DBAPI has been invoked correctly
+        expected_parameters = {
+            'cursor_factory': database.default_cursor_factory
+        }
+        expected_parameters.update(database.conn_details)
+        stub_connect.assert_called_once_with(**expected_parameters)
+
+        # Check the object return by the connect attempt is used.
+        mock_connection = stub_connect.return_value
+        mock_connection.set_session.assert_called_once_with(
+            autocommit=True,
+            readonly=True
+        )
+        self.assertIs(value, mock_connection)
 
 
 if __name__ == '__main__':
