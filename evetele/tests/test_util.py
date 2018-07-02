78254a7c1b92aba0dda091b66e09307ad3c5276e
Alex Corrie <ajccode@gmail.com>
2018-07-02 20:53:55 +0100

    Add date/time parsing utility functions.
    All functions treat dates and times as UTC because EVE time is in UTC.

--- evetele/tests/test_util.py
+++ evetele/tests/test_util.py
@@ -1,7 +1,11 @@
+import datetime
 import unittest
 from unittest import mock
 
-from ..util import cached_property
+import ddt
+import pytz
+
+from .. import util
 
 
 class TestCachedProperty(unittest.TestCase):
@@ -12,7 +16,7 @@ class TestCachedProperty(unittest.TestCase):
 
         class A(object):
             species = 'haddock'
-            @cached_property
+            @util.cached_property
             def x(self):
                 mock_object.method(fish=self.species)
                 return -1
@@ -25,6 +29,62 @@ class TestCachedProperty(unittest.TestCase):
         a.species = 'cod'
         a.x
         mock_object.method.assert_called_once_with(fish='haddock')
+
+
+@ddt.ddt
+class TestFunctions(unittest.TestCase):
+
+    @ddt.data(
+        ['20180702T182837+0000', datetime.date(2018, 7, 2)],
+        ['2018-07-02T00:28:37.000+0100', datetime.date(2018, 7, 1)],
+        ['2018-07-02T01:28:37.000+0100', datetime.date(2018, 7, 2)],
+        ['2018-07-02', datetime.date(2018, 7, 2)]
+    )
+    @ddt.unpack
+    def test_parse_date(self, inp, expected):
+        """A (UTC) date object is expected.
+
+        If the input is a timestamp rather than a simple datestamp,
+        the date is derived from the UTC datetime.
+        """
+        self.assertEqual(util.parse_date(inp), expected)
+
+    @ddt.data(
+        ['20180702T182837+0000',
+         datetime.datetime(2018, 7, 2, 18, 28, 37, tzinfo=pytz.utc)],
+        ['2018-07-02T18:28:37.000+0000',
+         datetime.datetime(2018, 7, 2, 18, 28, 37, tzinfo=pytz.utc)],
+        [datetime.datetime(2018, 7, 2, 18, 28, 37, tzinfo=pytz.utc),
+         datetime.datetime(2018, 7, 2, 18, 28, 37, tzinfo=pytz.utc)],
+        ['20180702T192837+0100',
+         datetime.datetime(2018, 7, 2, 18, 28, 37, tzinfo=pytz.utc)],
+    )
+    @ddt.unpack
+    def test_parse_datetime(self, inp, expected):
+        """A UTC datetime object is expected.
+
+        tzinfo is explicitly compared for the actual and expected
+        return values of the function.
+        """
+        actual = util.parse_datetime(inp)
+        self.assertEqual(actual, expected)
+        self.assertEqual(actual.tzinfo.utcoffset(actual),
+                         expected.tzinfo.utcoffset(expected))
+
+    @ddt.data(
+        [1530556413000, # milliseconds
+         datetime.datetime(2018, 7, 2, 18, 33, 33, tzinfo=pytz.utc)],
+        [1530556413,    # seconds
+         datetime.datetime(2018, 7, 2, 18, 33, 33, tzinfo=pytz.utc)],
+    )
+    @ddt.unpack
+    def test_parse_epoch_timestamp(self, inp, expected):
+        """A UTC datetime object is expected.
+
+        tzinfo is implicitly asserted here by the comparison with test
+        data that has it.
+        """
+        self.assertEqual(util.parse_epoch_timestamp(inp), expected)
 
 
 if __name__ == '__main__':
--- evetele/util.py
+++ evetele/util.py
@@ -1,4 +1,12 @@
+import collections
+import datetime
+import functools
 import logging
+import sys
+
+from dateutil import parser
+from dateutil.relativedelta import relativedelta as tdelta
+import pytz
 
 log = logging.getLogger(__name__)
 
@@ -29,3 +37,95 @@ def exception_logger(cls, inst, traceback):
     log.exception(': '.join([cls.__name__, str(inst)]))
     sys.__excepthook__(cls, inst, traceback)
 sys.excepthook = exception_logger
+
+
+def parse_date(obj):
+    """Convert a date-like object into a Python date object.
+
+
+    Parameters
+    ----------
+
+    obj : date-like object
+
+
+    Returns
+    -------
+
+    datetime.date
+
+
+    Notes
+    -----
+
+    UTC datetime is used where the object is datetime-like.
+    """
+    if isinstance(obj, datetime.date):
+        # no-op
+        return obj
+    elif isinstance(obj, str):
+        dt = parser.parse(obj)
+        dt = dt.replace(tzinfo=dt.tzinfo or pytz.utc) # UTC if naive
+        return dt.astimezone(pytz.utc).date()
+
+
+def parse_datetime(obj):
+    """Convert a datetime-like object into a Python UTC datetime.
+
+
+    Parameters
+    ----------
+
+    obj : datetime-like object
+
+
+    Returns
+    -------
+
+    datetime.datetime
+    """
+    if isinstance(obj, datetime.datetime):
+        dt = obj
+    else:
+        dt = parser.parse(obj)
+    return dt.astimezone(pytz.utc)
+
+
+def parse_epoch_timestamp(timestamp):
+    """Convert seconds or milliseconds since epoch to UTC datetime.
+
+    Units will be assumed based on the number of digits in the
+    timestamp.
+
+
+    Parameters
+    ----------
+
+    timestamp : int or str
+        UNIX timestamp
+
+
+    Returns
+    -------
+
+    datetime.datetime
+
+
+    Notes
+    -----
+
+    This assumes that the timestamp doesn't represent a date before
+    2001 (milliseconds have a 13-digit timestamp as far back as 2001).
+
+    Yes, guessing (or rather incorrectly guessing) units is a Bad Idea
+    but this is not the Mars Orbiter and luckily the timestamps we are
+    dealing with are in a nice, limited range.
+    """
+    if len(str(timestamp)) >= 13:
+        s = int(timestamp) // 1000
+        us = int(timestamp) % 1000 * 1000
+    else:
+        s = int(timestamp)
+        us = 0
+    converter = datetime.datetime.fromtimestamp
+    return converter(s, pytz.UTC).replace(microsecond=us)
